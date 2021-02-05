b269ce0026dd50e2b680173ebe5e7f169a3dfb7e
brenosekiro <b88660501@gmail.com>
2021-02-04 23:49:28 -0300

    appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:async';
 
 const String request = 'https://api.hgbrasil.com/finance?format=json&key=bad275ca'; 
 
 void main() async {
 
-  http.Response response = await http.get(request);
-  print(json.decode(response.body));
+  print(await getData());
 
   runApp(MaterialApp(
     title: "Conversor monetário",
-    home: Container(),
+    home: Home()
   ));
 }
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Coversor monetário"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+    );
+  }
+}
+
